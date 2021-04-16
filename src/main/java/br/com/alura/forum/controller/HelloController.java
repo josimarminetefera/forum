b68bc88cecf7d0cf29f21f752f0a0f32a62e01ec
josimarminetefera <josimaminete@gmail.com>
2021-04-16 11:41:06 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -0,0 +1,16 @@
+package br.com.alura.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	// localhost:8080
+	@RequestMapping("/")
+	@ResponseBody // para abrir na tela e não tentar encontrar uma pagina com o nome Ola Mundo
+	public String hello() {
+		return "Ola Mundo";
+	}
+}
