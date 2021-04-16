bc240f2ab32caf48e7cea31fb6d09c802135c7f4
josimarminetefera <josimaminete@gmail.com>
2021-04-16 14:55:04 -0300

    Mudando Controller para RestController

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,20 +3,19 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista() {
-		Topico topico = new Topico("Duvida", "Duvida em Spring", new Curso("Spring", "Programação"));
-		return Arrays.asList(topico, topico, topico);
+	public List<TopicoDto> lista() {
+		Topico topico = new Topico("Dúvida", "Duvida em Spring", new Curso("Spring", "Programação"));
+		return TopicoDto.topicoParaTopicoDto(Arrays.asList(topico, topico, topico, topico));
 	}
 }
