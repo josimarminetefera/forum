26595f6cca7b978888ca24b870f64dd19d33b944
josimarminetefera <josimaminete@gmail.com>
2021-05-04 17:17:05 -0300

    Adicionado parâmetro para ordenação de campo.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,17 +42,18 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	// http://localhost:8080/topicos
-	//http://localhost:8080/topicos?pagina=1&quantidade=1
+	// http://localhost:8080/topicos?pagina=1&quantidade=1
 	// http://localhost:8080/topicos?nomeCurso=HTML%205
 	// @RequestMapping(value = "/topicos", method = RequestMethod.GET)
 	// parametro vem na URL e não é obrigatório @RequestParam(required = false)
 	@GetMapping
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int quantidade) {
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina,
+			@RequestParam int quantidade, @RequestParam String ordenacao) {
+
 		System.out.println(nomeCurso);
-		
-		Pageable paginacao = PageRequest.of(pagina, quantidade);
-		
-		
+
+		Pageable paginacao = PageRequest.of(pagina, quantidade, Direction.ASC, ordenacao);
+
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDto.topicoParaTopicoDto(topicos);
