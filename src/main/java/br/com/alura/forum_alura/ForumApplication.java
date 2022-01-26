5aff451b1efaf87c949b04b11723bb19f74c3d86
Josimar Minete <josimaminete@gmail.com>
2022-01-25 21:45:05 -0300

    Revisa alguns arquivos

--- src/main/java/br/com/alura/forum_alura/ForumApplication.java
+++ src/main/java/br/com/alura/forum_alura/ForumApplication.java
@@ -2,14 +2,18 @@ package br.com.alura.forum_alura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport
+@EnableSpringDataWebSupport // isso aqui é para dar suporte a usar Pageable paginacao para não precisar
+							// passar @RequestParam int pagina, @RequestParam int quantidade, @RequestParam
+							// String ordenacao
 public class ForumApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ForumApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApplication.class, args);
+	}
 
 }
