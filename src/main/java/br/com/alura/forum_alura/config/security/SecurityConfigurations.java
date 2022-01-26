9933a01bf6a928e16dfd1f65dec4aade061315aa
Josimar Minete <josimaminete@gmail.com>
2022-01-26 19:59:14 -0300

    Configura liberação por URL

--- src/main/java/br/com/alura/forum_alura/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum_alura/config/security/SecurityConfigurations.java
@@ -1,11 +1,34 @@
 package br.com.alura.forum_alura.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity // Para habilitar o modulo de segurança na aplicação
 @Configuration // Para indicar class de configuração
-public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+	// Configuração de autenticação login acesso
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	}
+
+	// Configuração de autorizações perfis para discubrir o que tem acesso, quais
+	// url são publicas e quais privadas
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+	}
+
+	// Configuração de recursos estáticos permissão para arquivos js css imagens
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	}
 
 }
